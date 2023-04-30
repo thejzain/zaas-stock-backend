3a06ffc10165d76d9b2d58eb9f4d09f5a061eed2
Zain <thejzainkp@gmail.com>
2023-04-30 08:53:55 +0530

    sell added

--- index.js
+++ index.js
@@ -26,23 +26,62 @@ app.get("/getgraph", (req, res) => {
 
 app.post("/buy", (req, res) => {
   const body = req.body;
-  let array1 = [];
-  let array2 = [];
   console.log(body);
 
   fs.readFile("predictionOutput.csv", "utf8", function (err, data) {
     let array = data.split("\n");
+    let array1 = [];
+    let array2 = [];
     for (var i = 1; i < array.length - 1; i++) {
       var split = array[i].split(",");
       array1.push(split[0]);
       array2.push(split[1]);
     }
+    if (body.achivement < -20) {
+      res.json({ status: "bad" });
+    } else if (body.achivement < -5) {
+      res.json({ status: "if you will to" });
+    } else {
+      let flag = 0;
+      for (let i = 0; i < array2.length; i++) {
+        if (body.lastprice > array2[i]) {
+          flag++;
+        }
+        if (flag != 0) {
+          res.json({ status: "better to buy after " + i + 1 + "th month" });
+        } else {
+          res.json({ status: "no problem" });
+        }
+      }
+    }
   });
-  if (body.achivement < -20) {
-    res.json({ status: "bad" });
-  } else if (body.achivement < -5) {
-    res.json({ status: "if you will to" });
-  }
+});
+
+app.post("/sell", (req, res) => {
+  const body = req.body;
+  fs.readFile("output.csv", "utf8", function (err, data) {
+    console.log(data);
+    let array = data.split("\n");
+    let array1 = [];
+    let array2 = [];
+    for (var i = 1; i < array.length - 1; i++) {
+      var split = array[i].split(",");
+      array1.push(split[0]);
+      array2.push(split[1]);
+    }
+    for (let i = 0; i < array2.length; i++) {
+        let flag = 0;
+        if(body.lastprice < array2[i]){
+            flag++
+        }
+        if(flag != 0){
+            res.json({status : "better to sell after " + flag  + "months"})
+        } else {
+            res.json({status: "going to drop better to sell"})
+        }
+        
+    }
+  });
 });
 
 app.listen(port, () => {
